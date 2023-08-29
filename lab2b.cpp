512dcdeec13f30a9159ba3971bcb0cbf2944a94c
Trey Owen <treyeowen@gmail.com>
2023-08-29 15:56:56 -0500

    Added constants

---
+++ lab2b.cpp
@@ -0,0 +1,18 @@
+/*
+    Filename: lab2b.cpp
+    Authors: Trey Owen and Lucas
+    Date: 05 September 2023
+    Purpose: Do arithmetic to get a good grade
+*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    const int SURVEYED = 16500;
+    const double USE_ENERGY_DRINKS = .15;
+    const double PEFER_CITRUS = .58;
+
+    cout << "Num of people who drink more than one a week: " << SURVEYED;
+    return 0;
+}
